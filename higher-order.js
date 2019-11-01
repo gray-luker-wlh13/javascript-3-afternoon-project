0882db6aad5c330bd7120416ebd96cf19d0a51d2
grayluker05 <graysonluker@gmail.com>
2019-11-01 16:41:01 -0600

    finished all problems except for the last two nesting

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers  = mixedNumbers.filter(function(element){
+let evenNumbers = mixedNumbers.filter(function(element){
   return element % 2 === 0;
 })
 
@@ -46,7 +46,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices  = prices.map(function(element){
+let postTaxPrices = prices.map(function(element){
   return element *= 1.07
 });
 
@@ -67,7 +67,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(acc, curr){
+  return acc + curr;
+})
 
 
 
@@ -93,7 +95,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(e){
+  return e.CP > 200;
+})
 
 
 
@@ -110,7 +114,7 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map((e) => e.price + e.price * e.tax)
 
 
 
@@ -130,6 +134,7 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.filter(e => e.owner === 'Bob').reduce((prev, curr) => {return prev += curr.price}, 0)
+
 
 
