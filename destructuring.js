d04712d8875f4850cde227d3399a4bbbdfa09861
grayluker05 <graysonluker@gmail.com>
2019-11-01 13:37:12 -0600

    finished problems 1-4

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,7 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+const {color, make, model, year} = carDetails;
 
 
 
@@ -34,7 +34,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  const {title, firstName, lastName} = obj;
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,7 +53,11 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
+let totalPopulation = (obj) => {
+  let {utah, california, texas, arizona} = obj;
+  let total = utah + california + texas + arizona;
+  return total;
+}
 
 
 
@@ -67,7 +71,12 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
+let ingredients = (obj) => {
+  let foodType = [];
+  let {carb, fat, protein} = obj;
+  foodType.push(carb, fat, protein);
+  return foodType;
+}
 
 
 
